5301981f5177bf746cb09d026694393e1700ef96
suraj basnal <surajsnghbasnal@gmail.com>
2025-01-23 21:52:25 +0530

    Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { TodoProvider } from './Contexts'
+import TodoForm from './Components/TodoForm'
+import TodoItem from './Components/TodoItem'
 
 const App = () => {
     const [todos, setTodos] = useState([])
@@ -20,16 +22,31 @@ const App = () => {
         setTodos((prev) => prev.map((item) => item === id ? { ...item, completed: !item.completed } : item))
     }
 
+    useEffect(() => {
+        const todos = JSON.parse(localStorage.getItem("todos"))
+
+        if (todos && todos.length > 0) {
+            setTodos(todos)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
+
+
+
     return (
         <TodoProvider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
             <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */}
+                        <TodoForm />
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        <TodoItem />
                     </div>
                 </div>
             </div>
